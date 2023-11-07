e3dcd2bad0b3d17f71135461cacd69f7609ae01f
David Abram-Krismer <david.krismer71@gmail.com>
2023-11-07 14:54:31 -0800

    added handling for submissions

--- backend/controllers/form-api-controller.js
+++ backend/controllers/form-api-controller.js
@@ -1,11 +1,27 @@
 import formSchema from "../models/form-schema.js";
+import personSchema from "../models/person-schema.js";
 import mongoose from "mongoose";
 const formModel = mongoose.model("form");
+const personModel = mongoose.model("person")
 
 const addNewSubmission = async (req, res) => {
     try {
       let message = await formModel.create(req.body);
-      res.status(201).send(message);
+      res.status(201).send(req.body);
+
+      let menArray = [];
+      Object.entries(req.body).forEach(([group, groupValue]) => {
+        Object.entries(req.body[group].men).forEach(([person, personValue])=> {
+          menArray.push({
+            "group": group,
+            "reason": personValue.reason,
+            "problematic": personValue.problematic
+          })
+        })
+      })
+      console.log(menArray);
+      await personModel.insertMany(menArray);
+      
     } catch (err) {
       console.log(req.body);
       res.status(400).send(
@@ -14,5 +30,9 @@ const addNewSubmission = async (req, res) => {
       );
     }
   };
+
+  const addNewMan = async (req, res) => {
+
+  }
 
   export { addNewSubmission };
