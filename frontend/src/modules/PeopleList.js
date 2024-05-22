07e03996acfb22f75e023e94d2e4609fa8774bef
David Abram-Krismer <david.krismer71@gmail.com>
2024-05-22 10:37:05 -0700

    Removed option to delete men on review/reflect stage. Also changed wording to indicate this is a reflection process, not a technical one

--- frontend/src/modules/PeopleList.js
+++ frontend/src/modules/PeopleList.js
@@ -18,7 +18,6 @@ function PeopleList({ people, submitForm }) {
             {people.peopleData.map((person, index) => (
                 person.group === group.id ? (
                     <div className={`border-r-4 ${person.problematic === "yes" ? "border-yellow-500" : "border-green-500"} my-3 flex flex-row items-center text-justify`} key={person.nickname + person.group + person.reason}>
-                        <p className="mr-10 text-red-600 cursor-pointer font-bold" onClick={e => removePerson(people, index)}>X</p>
                         <div>
                             <p className="text-lg italic">{person.nickname}</p>
                             {person.problematic === "yes" ? <p className="text-sm italic">Problematic: {person.reason}</p> : <p className="text-sm italic">Safe</p>}
@@ -31,10 +30,10 @@ function PeopleList({ people, submitForm }) {
 
     return (
         <div className="container-md mx-8 p-5 text-center rounded flex flex-col border justify-center my-5 py-5 text-white text-xl">
-            <p className="text-3xl my-5 font-bold italic">Review</p>
-            <p className="mb-5">Here is a summary of what you've entered.</p>
-            <p className="mb-5">Reflect on what you've found and think carefully about the men you interact with.</p>
-            <p className="mb-5">Who might you avoid or eliminate from your social circle?</p>
+            <p className="text-3xl my-5 font-bold italic">Reflection</p>
+            <p className="mb-5">This is a summary of what you've reported.</p>
+            <p className="mb-5">Take some time to reflect on what you've found. Who adds to your life, and who detracts from it?</p>
+            <p className="mb-5">Who might you mindfully choose to spend more time with, and who might you avoid?</p>
             <hr/>
             <div className="my-5">
                 {GROUPS.map((group) => renderPeople(group))}
